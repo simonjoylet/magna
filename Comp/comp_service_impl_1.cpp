d38143c2aab2a9f8bc3d4d19871aee04a4efaaa9
Simon <youyouransjy@gmail.com>
2018-03-13 15:22:47 +0800

    创建Comp工程，修改Makefile，使其能一次生成四个不同的组件。

---
+++ Comp/comp_service_impl_1.cpp
@@ -0,0 +1,6 @@
+#include "comp_service_impl_0.cpp"
+
+int CompServiceImpl::Handle(const magna::AppRequest &req, magna::AppResponse *resp) {
+    return -1;
+}
+
---
+++ Comp/comp_service_impl_3.cpp
@@ -0,0 +1,5 @@
+#include "comp_service_impl_0.cpp"
+
+int CompServiceImpl::Handle(const magna::AppRequest &req, magna::AppResponse *resp) {
+	return -1;
+}
